f32289d923c03e04d6c016b9b16c7d3d18278427
YulijaU <cenite-moment@mail.ru>
2018-03-04 08:29:50 +0700

    Creates a schema & a script for generating json mock data

---
+++ buildScripts/mockDataSchema.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "type": "object",
+    "properties": {
+        "recepies": {
+            "type": "array",
+            "minItems": 3,
+            "maxItems": 5,
+            "items": {
+                "type": "object",
+                "properties": {                    
+                    "id": {
+                        "type": "number",
+                        "unique": true,
+                        "minimum": 1
+                    }, 
+                    "recepeName": {
+                        "type": "string"
+                    },
+                    "ingredients": {
+                        "type": "array",
+                        "items": {
+                          "type": "object",
+                          "properties": {
+                            "ingredientName": {
+                              "type": "string"
+                            }
+                          },
+                          required: ["ingredientName"]
+                        }
+                    }                    
+                },
+                required: ["id", "recepeName", "ingredients"]
+            }
+        }        
+    },
+    required: ["recepies"]
+};
