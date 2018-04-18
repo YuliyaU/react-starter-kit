d2f2c6ec3565bcdc16158d8c4af39a5e6357572b
YulijaU <yuliyaulanova0310@gmail.com>
2018-04-18 14:47:45 +0700

    Changes port declaration in [server.dev.js]

--- server.dev.js
+++ server.dev.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var path = require('path');
 
-var port = 3000;
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname, '../dist/index.html'));
@@ -10,7 +11,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/src'));
 
-app.listen(port, function(err) {
+app.listen(app.get('port'), function(err) {
     if(err) {
         console.log(err);
     } else {
